dd858c08585c02ec10603c0eb6fece8ac1fb251c
Dennis Taapken <dennis.taapken@easyflex.nl>
2018-05-09 14:18:20 +0200

    Updated build system

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-set -ev
+set -e
 archs="${ARCHS}"
 for addon in "$@"; do
+   echo -n "Building addon ${addon}... "
    if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
       if [ -z "$archs" ]; then
          archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ../${addon}/config.json)
       fi
 
       docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/../${addon}:/data homeassistant/amd64-builder ${archs} -t /data --no-cache
+      echo "done"
    else
-      echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
+      echo "skipped - no important change in commit range"
    fi
 done
