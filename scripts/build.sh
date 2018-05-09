670a663261c4ea17d1b8fc3bc7af8eff7c87a2a7
Dennis Taapken <dennis.taapken@easyflex.nl>
2018-05-09 14:56:54 +0200

    Fixed output of build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,12 @@ archs="${ARCHS}"
 for addon in "$@"; do
    echo -n "Building addon ${addon}... "
    if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
+      echo ""
       if [ -z "$archs" ]; then
          archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
       fi
 
       docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data --no-cache
-      echo "done"
    else
       echo "skipped - no important change found for this addon"
    fi
