2c0d3f7c4351e625116f24309c16de4c2c19fe35
Dennis Taapken <dennis.taapken@easyflex.nl>
2018-05-09 14:27:23 +0200

    Fixed path errors in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,10 @@ for addon in "$@"; do
    echo -n "Building addon ${addon}... "
    if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
       if [ -z "$archs" ]; then
-         archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ../${addon}/config.json)
+         archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
       fi
 
-      docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/../${addon}:/data homeassistant/amd64-builder ${archs} -t /data --no-cache
+      docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data --no-cache
       echo "done"
    else
       echo "skipped - no important change found for this addon"
