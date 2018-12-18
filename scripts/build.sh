2079e4c1e5093f9c6dec9cb047957228a1035ad5
Dennis Taapken <github@lioncode.com>
2018-12-18 15:21:36 +0100

    Added checks to prevent unnecessary builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,10 @@
 set -e
 archs="${ARCHS}"
 for addon in "$@"; do
+   echo "*****************************************************************************"
    echo "Building addon ${addon}... "
-   #if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
-      if [ -z "$archs" ]; then
+   if [[ -z ${TRAVIS_COMMIT_RANGE} ]] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
+      if [[ -z "$archs" ]]; then
          echo "Checking for archs in ${addon}/config.json..."
          archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64"] | join(" ")' ${addon}/config.json)
       fi
@@ -15,7 +16,7 @@ for addon in "$@"; do
          echo "============================================================================="
          docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder --${arch} -t /data --no-cache
       done
-   #else
-   #   echo "skipped - no important change found for this addon"
-   #fi
+   else
+      echo "skipped - no important change found for this addon"
+   fi
 done
