1e76feb8a1d89a4885f1e75d6c4a0cd6baa93e46
Dennis Taapken <dennis@lioncode.nl>
2018-12-27 10:08:04 +0100

    Refactored Dockerfile

--- find3/run.sh
+++ find3/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p /data/logs
 mkdir -p /data/data
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisor.conf
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
