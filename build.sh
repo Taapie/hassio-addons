a395a79c66b1eb822cb15c416ed84c3f2c98790b
Dennis Taapken <dennis@lioncode.nl>
2019-01-03 14:17:46 +0100

    Removed building of Go

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ archs="${ARCHS}"
 for addon in "$@"; do
    echo "*****************************************************************************"
    echo "Building addon ${addon}... "
-   #if [[ -z ${TRAVIS_COMMIT_RANGE} ]] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
+   if [[ -z ${TRAVIS_COMMIT_RANGE} ]] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
       if [[ -z "$archs" ]]; then
          echo "Checking for archs in ${addon}/config.json..."
          archs=$(jq -r '.arch // ["armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
       fi
 
       echo "Replacing {DATE}..."
-      sed -i.bak "s/{DATE}/$(date '+%Y%m%d')/g" ${addon}/config.json
+      sed -i.bak "s/{DATE}/$(date '+%Y%m%d%H%M')/g" ${addon}/config.json
 
       echo "Using archs: ${archs}"
 
@@ -20,7 +20,7 @@ for addon in "$@"; do
 
       echo "Reverting changes..."
       mv ${addon}/config.json.bak ${addon}/config.json
-   #else
-   #   echo "skipped - no important change found for this addon"
-   #fi
+   else
+      echo "skipped - no important changes found for this addon"
+   fi
 done
