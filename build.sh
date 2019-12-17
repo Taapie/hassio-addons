42ae0811f52c215934da36cdcfeb75754da9a42f
dennis.taapken <dennis@lioncode.nl>
2019-12-17 17:07:30 +0100

    Trying to fix docker build with other storage

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 set -e
+
+# If on Travis CI, update Docker's configuration.
+if [ "$TRAVIS" == "true" ]; then
+   echo "Fixing docker storage on Travic CI..."
+   mkdir /tmp/docker
+   echo '{
+      "experimental": true,
+      "storage-driver": "overlay2"
+   }' | sudo tee /etc/docker/daemon.json
+   sudo service docker restart
+   extra_docker_args="--volume=/etc/docker/daemon.json:/etc/docker/daemon.json:ro --mount type=bind,src=/tmp/docker,dst=/var/lib/docker"
+fi
+
 archs="${ARCHS}"
 for addon in "$@"; do
    echo "*****************************************************************************"
@@ -10,7 +23,7 @@ for addon in "$@"; do
       fi
 
       echo "============================================================================="
-      docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data 
+      docker run --rm --privileged ${extra_docker_args} -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data 
    else
       echo "skipped - no important changes found for this addon"
    fi
