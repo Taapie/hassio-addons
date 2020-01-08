304f6156b197cdb1aa9a2622d6add61327ee5151
Dennis Taapken <dennis.taapken@easyflex.nl>
2020-01-08 11:47:50 +0100

    Changed base image because of go build error

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 # If on Travis CI, update Docker's configuration.
 if [ "$TRAVIS" == "true" ]; then
+   echo "Running on Travis"
    echo "Fixing docker storage on Travic CI..."
    mkdir /tmp/docker
    echo '{
@@ -14,17 +15,35 @@ if [ "$TRAVIS" == "true" ]; then
    PWD=$(pwd)
    extra_docker_args="--volume=/etc/docker/daemon.json:/etc/docker/daemon.json:ro --mount type=bind,src=/tmp/docker,dst=/var/lib/docker --volume=$PWD:$DATA_PATH:rw"
    extra_builder_args=""
-else
+fi
+
+# If on HASS.IO make sure to use the share folder
+if [ "$HASSIO_TOKEN" != "" ]; then
+   echo "Running on Hass.io"
    DATA_PATH="/share/addon-build"
-   mkdir -p  ${DATA_PATH} > /dev/null 2>&1
+   rm -rf ${DATA_PATH} > /dev/null 2>&1
+   mkdir -p ${DATA_PATH} > /dev/null 2>&1
+   cp -pR . ${DATA_PATH}
    extra_docker_args="--volume=/mnt/data/supervisor/${DATA_PATH}:${DATA_PATH}:rw --volume=/var/run/docker.sock:/var/run/docker.sock:ro"
    extra_builder_args="--no-cache"
 fi
 
-# Determine locak machine architecture
+if [ "$DATA_PATH" == "" ]; then 
+   echo "Running on generic Docker machine"
+   # Set general settings
+   DATA_PATH="/data"
+   PWD=$(pwd)
+   extra_docker_args="--volume=/var/run/docker.sock:/var/run/docker.sock:ro --volume=$PWD:$DATA_PATH:rw"
+   extra_builder_args="--no-cache"
+fi
+
+# Determine local machine architecture
 LOCAL_ARCH=`uname -m`
 if [[ $LOCAL_ARCH == "armv7l" ]]; then
   LOCAL_ARCH='armv7'
+fi
+if [[ $LOCAL_ARCH == "x86_64" ]]; then
+  LOCAL_ARCH='amd64'
 fi
 
 archs="${ARCHS}"
@@ -37,10 +56,6 @@ for addon in "$@"; do
       fi
 
       echo "============================================================================="
-      if [ "$TRAVIS" != "true" ]; then
-         rm -rf  ${DATA_PATH}/${addon} > /dev/null 2>&1
-         cp -pR ${addon} ${DATA_PATH}
-      fi
       docker run --rm --privileged ${extra_docker_args} --volume ~/.docker:/root/.docker homeassistant/${LOCAL_ARCH}-builder ${extra_builder_args} -t ${DATA_PATH}/${addon} ${archs}
    else
       echo "skipped - no important changes found for this addon"
