1fb843eb7151bdd31f16e16e3b3f585457d56b15
Dennis Taapken <dennis.taapken@easyflex.nl>
2020-01-10 15:33:25 +0100

    Try different arch setup for manifest

--- build.sh
+++ build.sh
@@ -66,7 +66,7 @@ for ADDON in "$@"; do
 	    docker manifest create $IMAGE:$VERSION $DOCKER_IMAGES
             for DOCKER_IMAGE in $DOCKER_IMAGES; do
                PLATFORM=${DOCKER_IMAGE##*-}
-               docker manifest annotate $IMAGE:$VERSION $DOCKER_IMAGE --arch linux/$PLATFORM
+               docker manifest annotate $IMAGE:$VERSION $DOCKER_IMAGE --arch $PLATFORM
             done
             docker manifest push $IMAGE:$VERSION
          fi
