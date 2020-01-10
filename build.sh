fd31265f68e701ae421260b54c6014d5e5f7316a
Dennis Taapken <dennis.taapken@easyflex.nl>
2020-01-10 13:09:03 +0100

    Multiarch using Travis

--- build.sh
+++ build.sh
@@ -1,63 +1,47 @@
 #!/usr/bin/env bash
 set -e
 
-# If on Travis CI, update Docker's configuration.
-if [ "$TRAVIS" == "true" ]; then
-   echo "Running on Travis"
-   echo "Fixing docker storage on Travic CI..."
-   mkdir /tmp/docker
-   echo '{
-      "experimental": true,
-      "storage-driver": "overlay2"
-   }' | sudo tee /etc/docker/daemon.json > /dev/null
-   sudo service docker restart
-   DATA_PATH="/data"
-   PWD=$(pwd)
-   extra_docker_args="--volume=/etc/docker/daemon.json:/etc/docker/daemon.json:ro --mount type=bind,src=/tmp/docker,dst=/var/lib/docker --volume=$PWD:$DATA_PATH:rw"
-   extra_builder_args=""
-fi
+export DOCKER_CLI_EXPERIMENTAL=enabled
 
-# If on HASS.IO make sure to use the share folder
-if [ "$HASSIO_TOKEN" != "" ]; then
-   echo "Running on Hass.io"
-   DATA_PATH="/share/addon-build"
-   rm -rf ${DATA_PATH} > /dev/null 2>&1
-   mkdir -p ${DATA_PATH} > /dev/null 2>&1
-   cp -pR . ${DATA_PATH}
-   extra_docker_args="--volume=/mnt/data/supervisor/${DATA_PATH}:${DATA_PATH}:rw --volume=/var/run/docker.sock:/var/run/docker.sock:ro"
-   extra_builder_args="--no-cache"
-fi
-
-if [ "$DATA_PATH" == "" ]; then 
-   echo "Running on generic Docker machine"
-   # Set general settings
-   DATA_PATH="/data"
-   PWD=$(pwd)
-   extra_docker_args="--volume=/var/run/docker.sock:/var/run/docker.sock:ro --volume=$PWD:$DATA_PATH:rw"
-   extra_builder_args="--no-cache"
-fi
-
-# Determine local machine architecture
-LOCAL_ARCH=`uname -m`
-if [[ $LOCAL_ARCH == "armv7l" ]]; then
-  LOCAL_ARCH='armv7'
-fi
-if [[ $LOCAL_ARCH == "x86_64" ]]; then
-  LOCAL_ARCH='amd64'
-fi
-
-archs="${ARCHS}"
-for addon in "$@"; do
+for ADDON in "$@"; do
    echo "*****************************************************************************"
-   echo "Building addon ${addon}... "
-   if [[ ${BUILD} == "true" ]] || [[ -z ${TRAVIS_COMMIT_RANGE} ]] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
-      if [ -z "$archs" ]; then
-    	archs=$(jq -r '.arch // ["armv7", "armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
-      fi
+   echo "Building addon ${ADDON}... "
+   if [[ ${BUILD} == "true" ]] || [[ -z ${TRAVIS_COMMIT_RANGE} ]] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${ADDON}; then
+       ARCHS=$(jq -r '.arch // ["armv7", "armhf", "amd64", "aarch64", "i386"] | [.[] | .] | join(" ")' ${ADDON}/config.json)
+       VERSION=$(jq -r '.version' ${ADDON}/config.json)
+       IMAGE=$(jq -r '.image' ${ADDON}/config.json)
 
-      echo "============================================================================="
-      #docker run --rm --privileged ${extra_docker_args} --volume ~/.docker:/root/.docker homeassistant/${LOCAL_ARCH}-builder ${extra_builder_args} -t ${DATA_PATH}/${addon} ${archs}
-      docker buildx ls
+      for ARCH in $ARCHS; do
+         echo "============================================================================="
+	 echo "Build for architecture '$ARCH'"
+
+         case "${ARCH}" in 
+            amd64) PLATFORM=adm64 ;;
+            armhf) PLATFORM=arm ;;
+            armv7) PLATFORM=arm ;;
+            aarch64) PLATFORM=arm64 ;;
+            i386) PLATFORM=386 ;;
+         esac
+	 
+         DOCKERFILE_LOCATION="${ADDON}/Dockerfile"
+	 DOCKER_IMAGE=${IMAGE}
+	 DOCKER_TAG=${VERSION}
+
+	 BUILD_ARCH=${ARCH}
+	 BUILD_VERSION=${VERSION}
+         BUILD_FROM=$(jq -r ".build_from.${ARCH}" ${ADDON}/config.json)
+
+	 env
+
+         buildctl build --frontend dockerfile.v0 \
+                        --frontend-opt platform=linux/${PLATFORM} \
+                        --frontend-opt filename=${DOCKERFILE_LOCATION} \
+                        --exporter image \
+                        --exporter-opt name=docker.io/${DOCKER_IMAGE}:${DOCKER_TAG} \
+                        --exporter-opt push=true \
+                        --local dockerfile=. \
+                        --local context=.	 
+      done
    else
       echo "skipped - no important changes found for this addon"
    fi
