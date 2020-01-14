7fac2e7836597e0859682197cbdb61a456d5c6a9
dennis.taapken <dennis@lioncode.nl>
2020-01-14 16:54:47 +0100

    Added backup

--- tuya-convert/run.sh
+++ tuya-convert/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cd /data/tuya-convert
+mkdir -p /config/tuya-convert/backups > /dev/null 2>&1
+ln -s /config/tuya-convert/backups backups
 touch scripts/eula_accepted
 ./start_flash.sh
