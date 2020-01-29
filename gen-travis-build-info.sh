17943c9e764976ec6ff9f9bfd8f22a7e01d23d70
dennis.taapken <dennis@lioncode.nl>
2020-01-29 15:38:26 +0100

    Added label

--- gen-travis-build-info.sh
+++ gen-travis-build-info.sh
@@ -13,11 +13,9 @@ for ADDON in $ADDONS; do
    if [[ -f "${ADDON}/${CONFIG}" ]]; then 
       ARCHS=$(jq -r '.arch // ["armv7", "armhf", "amd64", "aarch64", "i386"] | [.[] | .] | join(" ")' ${ADDON}/${CONFIG})
       for ARCH in $ARCHS; do
-         ENV="$ENV  - ADDON=\"$ADDON\" ARCH=\"$ARCH\"\n"
+         ENV="$ENV  - LABEL=\"$ADDON-$ARCH\" ADDON=\"$ADDON\" ARCH=\"$ARCH\" CONFIG=\"$CONFIG\"\n"
       done
    fi
 done
-
-echo $ENV
 
 sed -i -e "/#START_GEN_ENV/,/#END_GEN_ENV/c\#START_GEN_ENV\n$ENV#END_GEN_ENV" .travis.yml
