39a999126e931bd1257b140088c69dfea23973db
dennis.taapken <dennis@lioncode.nl>
2020-01-30 13:14:45 +0100

    Set BUILD to true

--- gen-travis-build-info.sh
+++ gen-travis-build-info.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+EXTRA_ENV="$*"
+
 BADGE_URL="https://badges.herokuapp.com/travis/Taapie/hassio-addons?branch=${BRANCH}&label=${LABEL}&env=ADDON=%22$ADDON%22%20ARCH=%22$ARCH%22"
 BRANCH=`git branch --show-current`
 
 BUILD_PART="    - stage: build\n      env:\n        - \$ENV\n      script:\n        - docker login -u \$DOCKER_USER -p \$DOCKER_PASSWORD\n        - ARCHS=\$ARCH ./travis-build.sh \$ADDON\n"
 
-DONE_PART="    - stage: manifest\n      env:\n        - \$ENV\n      script:\n        - docker login -u \$DOCKER_USER -p \$DOCKER_PASSWORD\n        - ./travis-set-mainfest.sh \$ADDON\n"
+DONE_PART="    - stage: manifest\n      env:\n        - \$ENV\n      script:\n        - docker login -u \$DOCKER_USER -p \$DOCKER_PASSWORD\n        - ./travis-set-manifest.sh \$ADDON\n"
 
 ADDONS=`ls -d */ | cut -d/ -f 1`
 for ADDON in $ADDONS; do
@@ -19,9 +21,9 @@ for ADDON in $ADDONS; do
    BADGES=""
    if [[ -f "${ADDON}/${CONFIG}" ]]; then 
       ARCHS=$(jq -r '.arch // ["aarch64", "amd64", "armhf", "armv7", "i386"] | [.[] | .] | sort | join(" ")' ${ADDON}/${CONFIG})
-      DONE_ENV="$DONE_ENV${DONE_PART/\$ENV/ADDON=\"$ADDON\" ARCHS=\"$ARCHS\" CONFIG=\"$CONFIG\"}"
+      DONE_ENV="$DONE_ENV${DONE_PART/\$ENV/ADDON=\"$ADDON\" ARCHS=\"$ARCHS\" CONFIG=\"$CONFIG\" ${EXTRA_ENV}}"
       for ARCH in $ARCHS; do
-         BUILD_ENV="$BUILD_ENV${BUILD_PART/\$ENV/ADDON=\"$ADDON\" ARCH=\"$ARCH\" CONFIG=\"$CONFIG\"}"
+         BUILD_ENV="$BUILD_ENV${BUILD_PART/\$ENV/ADDON=\"$ADDON\" ARCH=\"$ARCH\" CONFIG=\"$CONFIG\" ${EXTRA_ENV}}"
 	 LABEL="$ARCH"
          BADGE_URL="https://badges.herokuapp.com/travis/Taapie/hassio-addons?branch=${BRANCH}&label=${LABEL}&env=ADDON=%22${ADDON}%22%20ARCH=%22${ARCH}%22"
 	 BADGE_MD="[![Build Status]($BADGE_URL)](https://travis-ci.org/Taapie/hassio-addons)"
